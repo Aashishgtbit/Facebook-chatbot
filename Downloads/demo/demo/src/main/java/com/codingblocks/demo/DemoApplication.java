de43c8be5cc946dc53e1b391bbe3d6f27486e2ea
Aashishgtbit <aashishtiwari97@gmail.com>
2017-09-03 16:28:28 +0530

    This is my first chatbot

---
+++ Downloads/demo/demo/src/main/java/com/codingblocks/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.codingblocks.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@SpringBootApplication
+public class DemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+
+	}
+}
---
+++ Downloads/demo/demo/src/main/java/com/codingblocks/demo/Hook.java
@@ -0,0 +1,13 @@
+package com.codingblocks.demo;
+
+public class Hook {
+    item [] entry;
+    public static class item{
+    Content[] messaging;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
